bcca5a6954e1bd0d9eae900ab98c700213ec6b76
Flávia Monique <flaviamonique38@gmail.com>
2024-12-11 21:53:45 -0300

    rotas delete, patch e post

--- rotas/livros.js
+++ rotas/livros.js
@@ -6,5 +6,15 @@ const router = Router()
 router.get('/', (req,res) => {
     res.send("Olá mundo eeeee")})
 
+router.post('/', (req, res) => {
+   res.send('Você fez uma requisição do tipo post') 
+})
+
+router.patch('/', (req,res) => {
+    res.send("voce fez uma requisição do tipo patch")})
+
+router.delete('/', (req, res) => {
+   res.send('Você fez uma requisição do tipo delete') 
+})
 module.exports = router
 //exporta essa rota
